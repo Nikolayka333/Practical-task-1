84609109fa18631929c096026dfdc938d97f6029
Nikolayka333 <nikolayka333@gmail.com>
2024-11-06 15:13:01 +0300

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,8 @@
+#4th program
+print('123.456')
+print(type('123.456'))
+print(float('123.456'))
+print(type(float('123.456')))
+print(float('123.456') * 10)
+print(int(float('123.456') * 10))
+print(int(float('123.456') * 10) % 10) #выполнил условие задачи
